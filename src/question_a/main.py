b07ebb020d47591d56909363c1cd6022b724f1b9
alguacil803 <143223663+alguacil803@users.noreply.github.com>
2023-10-14 13:45:36 +0000

    Mid term 55 tray

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,16 +1,3 @@
 import question_a
-again = "yes" 
 
-while again == "yes" or again == "y" or again == "Y" :
-
-    num = input("ENTER A NUMBER ")
-    num = int (num)
-    y = question_a.is_prime(num)
-
-    print (y)
-
-    again = str(input("if you want to try again , Enter yes .If not enter any other character: "))
-
-    if again != "yes" and again != "y" and again != "Y":
-
-        print ("exiting program")
+question_a.is_prime_menu()
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -6,10 +6,17 @@ from src.question_a.question_a import test_config
 
 
 #follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import get_person_category
-from src.question_b.question_b import get_fahrenheit
-from src.question_c.question_c import get_bonus_pay_amount
+from src.question_a.question_a import is_prime
+from src.question_b.question_b import get_miles_per_hour
+from src.question_c.question_c import get_sum_of_evens
 from src.question_d.question_d import get_day_of_week
 
 class Test_Config(unittest.TestCase):
+
+
+    def test_is_prime(self):
+        self.assertEqual(is_prime(4), False)
+        self.assertEqual(is_prime(5), True)
+        self.assertEqual(is_prime(11), True)
+
 
